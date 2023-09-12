2df814c066464ec38efc5da532d69e1aece6479f
Joaquin Imwinkelried <imwinkelriedjoaquin@gmail.com>
2023-09-12 16:07:33 -0300

    Tercer Commit (Foro funcionando)

--- readme.md
+++ readme.md
@@ -1,8 +1,7 @@
 # Elaboracion del foro en proceso,
 
-por ahora solo permite la creacion y la eliminacion de posteos
+El foro ya permite la creacion, edicion y eliminacion de Posteos
 
-todavia me encuentro trabajando en la edicion de los post ya que desde el postman se edita perfecto, pero por alguna razon en la web no esta afectando a la base de datos
 
 Hay que crear el archivo .env indicando el puerto que corresponda, usuario y contraseña de MySQL tal como se ve en el .ejemplo.env
 
--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -17,14 +17,32 @@ const listPosts=async(req=request,res=response)=>{
     res.json(allPosts);
 }
 
-const acturalizarPost = async (req = request, res = response) => {
-    const {id}=req.params;
-    const { title, content, author, img } = req.body
-    const post = { title, content, author, img }
-    await PostModel.update(post,{where: { id: id }})
+const acturalizarPost = async (req, res) => {
+    const { id } = req.params;
 
-    res.redirect('/')
+    // acá con esto válidas que exista el id que envías antes de querer actualizar, sino te arroja un error. 
+    if (!id) {
+        return res.status(400).json({ error: 'El ID es requerido' });
+    }
 
+    const { title, content, author, img } = req.body;
+    const post = { title, content, author, img };
+    
+
+    try {
+      //acá haces el try, intentas actualizar el post.
+        const [updatedRows] = await PostModel.update(post, { where: { id: id } });
+
+        //acá se verifica de nuevo Si no se actualizó ninguna fila, es posible que el ID no existe
+        if (!updatedRows) {
+            return res.status(404).json({ error: 'No se encontró el post con ese ID', msg:id });
+        }
+
+        return res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Error al actualizar el post'});
+    }
 };
 
 const borrarPost=async(req=request, res=response)=> {
